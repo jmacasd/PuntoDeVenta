cb89c8fc159da590bc0511d8b329110a8e8998ef
José A. Macas D. <63927290+jmacasd@users.noreply.github.com>
2024-05-13 19:47:17 -0700

    Implementación de métodos asíncronos en SQLServer de acceso a datos

--- AccesoDatos/SQLServer.cs
+++ AccesoDatos/SQLServer.cs
@@ -43,6 +43,36 @@ namespace AccesoDatos
             }
         }
 
+        public async Task NonQueryAsync(string query, SqlParameter[]? parameters = null)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.CommandType = CommandType.Text;
+
+                        if (parameters != null)
+                        {
+                            command.Parameters.AddRange(parameters);
+                        }
+
+                        await connection.OpenAsync();
+
+                        if (await command.ExecuteNonQueryAsync() == 0)
+                        {
+                            throw new Exception("No hubo cambios en registros.");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
         public T? Reader<T>(string query, SqlParameter[]? parameters = null) where T : class, new()
         {
             try
@@ -67,18 +97,22 @@ namespace AccesoDatos
                                 T resultType = new ();
                                 
                                 int count = dataReader.FieldCount - 1;
+                                
                                 for (int i = 0; i <= count; i++)
                                 {
                                     string columna = dataReader.GetName(i);
                                     
                                     PropertyInfo propiedad = typeof(T).GetProperty(columna);
+                                    
                                     if (propiedad != null && !dataReader.IsDBNull(i))
                                     {
                                         propiedad.SetValue(resultType, dataReader.GetValue(i));
                                     }
                                 }
+                                
                                 return resultType;
                             }
+                           
                             return null;
                         }
                     }
@@ -87,6 +121,57 @@ namespace AccesoDatos
             catch (Exception exception)
             {
                 throw new Exception(exception.Message);
+            }
+        }
+
+        public async Task<T?> ReaderAsync<T>(string query, SqlParameter[]? parameters = null) where T : new()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.CommandType = CommandType.Text;
+
+                        if (parameters != null)
+                        {
+                            command.Parameters.AddRange(parameters);
+                        }
+
+                        await connection.OpenAsync();
+
+                        using (SqlDataReader dataReader = await command.ExecuteReaderAsync())
+                        {
+                            if (await dataReader.ReadAsync())
+                            {
+                                T resulType = new ();
+                                
+                                int count = dataReader.FieldCount - 1;
+                                
+                                for (int i = 0; i <= count; i++)
+                                {
+                                    string columna = dataReader.GetName(i);
+                                    
+                                    PropertyInfo propiedad = typeof(T).GetProperty(columna)!;
+
+                                    if (propiedad != null && !dataReader.IsDBNull(i))
+                                    {
+                                        propiedad.SetValue(resulType, dataReader.GetValue(i));
+                                    }
+                                }
+                                
+                                return resulType;
+                            }
+
+                            return default(T);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
             }
         }
 
@@ -120,15 +205,18 @@ namespace AccesoDatos
                                     string nombreColumna = dataReader.GetName(i);
                                     
                                     PropertyInfo propiedad = typeof(T).GetProperty(nombreColumna);
+                                    
                                     if (propiedad != null && !dataReader.IsDBNull(i))
                                     {
                                         object valor = dataReader.GetValue(i);
+                                        
                                         propiedad.SetValue(resultType, valor);
                                     }
                                 }
 
                                 lista.Add(resultType);
                             }
+                            
                             return lista;
                         }
                     }
@@ -137,6 +225,59 @@ namespace AccesoDatos
             catch (Exception exception)
             {
                 throw new Exception(exception.Message);
+            }
+        }
+
+        public async Task<List<T>?> ReaderListAsync<T>(string query, SqlParameter[]? parameters = null) where T : new()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.CommandType = CommandType.Text;
+
+                        if (parameters != null)
+                        {
+                            command.Parameters.AddRange(parameters);
+                        }
+
+                        await connection.OpenAsync();
+
+                        using (SqlDataReader dataReader = await command.ExecuteReaderAsync())
+                        {
+                            List<T> lista = new List<T>();
+
+                            while (await dataReader.ReadAsync())
+                            {
+                                T objeto = new T();
+
+                                for (int i = 0; i < dataReader.FieldCount; i++)
+                                {
+                                    string nombreColumna = dataReader.GetName(i);
+                                    
+                                    PropertyInfo propiedad = typeof(T).GetProperty(nombreColumna);
+
+                                    if (propiedad != null && !dataReader.IsDBNull(i))
+                                    {
+                                        object valor = dataReader.GetValue(i);
+                                        
+                                        propiedad.SetValue(objeto, valor);
+                                    }
+                                }
+
+                                lista.Add(objeto);
+                            }
+                            
+                            return lista;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
             }
         }
 
@@ -158,6 +299,35 @@ namespace AccesoDatos
                         connection.Open();
                         
                         return (T)command.ExecuteScalar();
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exception.Message);
+            }
+        }
+
+        public async Task<T> ScalarAsync<T>(string query, SqlParameter[]? parameters = null)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.CommandType = CommandType.Text;
+
+                        if (parameters != null)
+                        {
+                            command.Parameters.AddRange(parameters);
+                        }
+
+                        await connection.OpenAsync();
+
+                        var result = await command.ExecuteScalarAsync();
+                        
+                        return (T)result!;
                     }
                 }
             }
